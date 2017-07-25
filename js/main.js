36d5582797c2ef2e539238f2a04784125e8e1936
cstapiarubio <cstapiarubio@gmail.com>
2017-07-24 21:01:03 -0400

    listo nav y slider

---
+++ js/main.js
@@ -0,0 +1,23 @@
+/* fx carousel*/
+
+// start carrousel
+
+   $('.carousel.carousel-slider').carousel({
+      fullWidth: true,
+      indicators: false
+   });
+
+
+   // move next carousel
+   $('.moveNextCarousel').click(function(e){
+      e.preventDefault();
+      e.stopPropagation();
+      $('.carousel').carousel('next');
+   });
+
+   // move prev carousel
+   $('.movePrevCarousel').click(function(e){
+      e.preventDefault();
+      e.stopPropagation();
+      $('.carousel').carousel('prev');
+   });
